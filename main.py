a9944fff358e3dcac2d4645fd523e4defd7f6192
Adnan Saeed <adnansaeed.datascientist@gmail.com>
2026-05-06 11:00:00 +0500

    initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from foodi!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ my-app/src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="text-white font">
+      <h2>HOME PAGE</h2>
+    </div>
+  );
+}
